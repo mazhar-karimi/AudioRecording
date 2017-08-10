2b49521a6c3b05a4515cb5abbae2af37e2348ff3
mazhar-karimi <mazhar.karimi@gmail.com>
2017-08-10 18:38:16 +0500

    added new project for audio recording and show.

---
+++ Sound1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Sound1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Recording";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Recording";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 86);
+            this.textBox1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(34, 144);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(811, 323);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 539);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+ 
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Sound1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+
+namespace Sound1
+{
+    public partial class Form1 : Form
+    {
+        int n = 4000; // number of x-axis pints
+        WaveIn s_WaveIn;
+        private FileStream memoryStream;
+        private WaveFileWriter writer;
+        Queue<double> myQ;
+        public Form1()
+        {
+            InitializeComponent();
+
+            myQ = new Queue<double>(Enumerable.Repeat(0.0, n).ToList()); // fill myQ w/ zeros
+            chart1.ChartAreas[0].AxisY.Minimum = -10000;
+            chart1.ChartAreas[0].AxisY.Maximum = 10000;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (memoryStream == null)
+                memoryStream = new FileStream(System.Environment.CurrentDirectory + "\\" +
+                    Guid.NewGuid().ToString().Replace("-", "") + ".wav", FileMode.CreateNew);
+
+            s_WaveIn = new WaveIn();
+            s_WaveIn.WaveFormat = new WaveFormat(44100, 2);
+
+            s_WaveIn.BufferMilliseconds = 1000;
+            s_WaveIn.DataAvailable += new EventHandler<WaveInEventArgs>(SendCaptureSamples);
+            this.writer = new WaveFileWriter(this.memoryStream, this.s_WaveIn.WaveFormat);
+            s_WaveIn.RecordingStopped += s_WaveIn_RecordingStopped;
+            s_WaveIn.StartRecording();
+            timer2.Enabled = true;
+
+        }
+
+        void s_WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (s_WaveIn != null)
+            {
+                s_WaveIn.Dispose();
+                s_WaveIn = null;
+            }
+
+            if (writer != null)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+        }
+
+        void SendCaptureSamples(object sender, WaveInEventArgs e)
+        {
+            for (int i = 0; i < e.BytesRecorded; i += 2)
+            {
+                myQ.Enqueue(BitConverter.ToInt16(e.Buffer, i));
+                myQ.Dequeue();
+            }
+
+            if (writer != null)
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                writer.Flush();
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s_WaveIn.StopRecording();
+            if (memoryStream != null)
+            {
+                memoryStream.Close();
+                memoryStream.Dispose();
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                chart1.Series["Series1"].Points.DataBindY(myQ);
+                //chart1.ResetAutoValues();
+            }
+            catch
+            {
+                Console.WriteLine("No bytes recorded");
+            }
+        }
+
+
+    }
+}
